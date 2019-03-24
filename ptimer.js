08b6d73db1e33cc61fc0109961380e82e140aaef
William McCrackin <billymccrackin123@gmail.com>
2019-03-23 23:30:13 -0600

    adjustable timer settings

--- ptimer.js
+++ ptimer.js
@@ -1,36 +1,75 @@
-let btn = document.getElementById('playButton');
-let timeLeft = document.getElementById('time-left');
-let workTime = document.getElementById('workVal');
-let breakTime = document.getElementById('breakVal');
-let incWork = document.getElementById('addWork');
-let decWork = document.getElementById('minusWork');
-let incBreak = document.getElementById('addBreak');
+const btn = document.getElementById('playButton');
+const workTime = document.getElementById('workVal');
+const breakTime = document.getElementById('breakVal');
+const incWork = document.getElementById('addWork');
+const decWork = document.getElementById('minusWork');
+const incBreak = document.getElementById('addBreak');
+const decBreak = document.getElementById('minusBreak');
+let timeMin = document.getElementById('time-min');
 
 function incWorkTime(){
     if( workTime.innerText < 45){
         ++workTime.innerText
+        ++timeMin.innerText
     }
-}
+};
 function decWorkTime(){
     if( workTime.innerText > 10){
         --workTime.innerText
+        --timeMin.innerText
     }
-}
+};
 function incBreakTime(){
     if( breakTime.innerText < 15){
         ++breakTime.innerText
     }
-}
-
+};
+function decBreakTime(){
+    if( breakTime.innerText > 5){
+        --breakTime.innerText
+    }
+};
 function myFunction(){
     btn.classList.toggle('button-paused')
-}
+};
 
 function stopFunction() {
     btn.classList.replace('button-paused', 'button-play')
+};
+
+incWork.addEventListener('click',() => incWorkTime());
+decWork.addEventListener('click',() => decWorkTime());
+incBreak.addEventListener('click', () => incBreakTime());
+decBreak.addEventListener('click', () => decBreakTime());
+
+
+
+//timer functions
+const timerDisplay = document.querySelector('#session')
+
+
+let countdown;
+function timer(seconds){
+    const now = Date.now();
+    const then = now + seconds * 1000
+    displayTimeLeft(seconds)
+    
+    countdown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        if(secondsLeft < 0){
+            clearInterval(countdown);
+            return;
+        }
+       
+        displayTimeLeft(secondsLeft);
+    }, 1000);
 }
+function displayTimeLeft(seconds){
+    const minutes = Math.floor(seconds / 60);
+    const secondsRemainder = Math.floor(seconds % 60);
+    const display = `${minutes}:${secondsRemainder < 10 ? "0" : ""}${
+    secondsRemainder}`
 
-incWork.addEventListener('click',() => incWorkTime())
-decWork.addEventListener('click',() => decWorkTime())
-incBreak.addEventListener('click', () => incBreakTime())
-
+    timerDisplay.textContent = display;
+    console.log({minutes, secondsRemainder});
+}
