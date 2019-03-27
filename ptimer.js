f0533a69ab0c2a783b59b008ddc23a5fd1f2eff1
William McCrackin <billymccrackin123@gmail.com>
2019-03-27 11:59:27 -0600

    Functional timer with buzzer

--- ptimer.js
+++ ptimer.js
@@ -5,36 +5,65 @@ const incWork = document.getElementById('addWork');
 const decWork = document.getElementById('minusWork');
 const incBreak = document.getElementById('addBreak');
 const decBreak = document.getElementById('minusBreak');
-const timerDisplay = document.querySelector('#session');
+let timerDisplay = document.querySelector('#session');
 let timeMin = document.getElementById('time-min');
 let timeSec = document.getElementById('time-sec');
+let stopBtn = document.getElementById('stopButton');
+let switchBreak = document.getElementById('switchToBreak');
+let switchWork = document.getElementById('switchToWork');
 let btnPaused;
 let minPaused;
 let secPaused;
 let pausedAt;
 let isplaying;
 let countdown;
+let isWork;
+let isBreak;
+let hasPlayed;
 
 function incWorkTime(){
     if( workTime.innerText < 45){
-        ++workTime.innerText
-        ++timeMin.innerText
+        if(hasPlayed == 0){
+            ++workTime.innerText;
+            timerDisplay.innerText = `${workTime.innerText}:00`
+        }else{
+            ++workTime.innerText
+            ++timeMin.innerText
+        }
     }
 };
 function decWorkTime(){
-    if( workTime.innerText > 10){
-        --workTime.innerText
-        --timeMin.innerText
+    if( workTime.innerText > 10 || workTime.innerText <= 10){
+        if(hasPlayed == 0){
+            --workTime.innerText;
+            timerDisplay.innerText = `${workTime.innerText}:00`
+        }else{
+            --workTime.innerText
+            --timeMin.innerText
+        }
     }
 };
 function incBreakTime(){
     if( breakTime.innerText < 15){
-        ++breakTime.innerText
+        if(hasPlayed == 0){
+            ++breakTime.innerText
+            timerDisplay.innerText = `${breakTime.innerText}:00`
+        }else{
+            ++breakTime.innerText
+            ++timeMin.innerText
+        }
     }
 };
 function decBreakTime(){
     if( breakTime.innerText > 5){
-        --breakTime.innerText
+        if(hasPlayed == 0){
+            --breakTime.innerText
+            timerDisplay.innerText = `${breakTime.innerText}:00`
+
+        }else{
+            --breakTime.innerText
+            --timeMin.innerText
+        }
     }
 };
 function myFunction(){
@@ -42,7 +71,9 @@ function myFunction(){
 };
 
 function stopFunction() {
-    btn.classList.replace('button-paused', 'button-play')
+    if(isplaying == true){
+    location.reload();
+    }
 };
 
 //timer functions
@@ -55,6 +86,13 @@ function timer(seconds){
         let secondsLeft = Math.round((then - Date.now()) / 1000);
         if(secondsLeft < 0){
             clearInterval(countdown);
+            timesUp();
+            if(isWork == true){
+                getBreak();
+            }
+            else if(isBreak == true){
+                getWork();
+            }
             return;
         }
        
@@ -89,6 +127,36 @@ function startTimer(){
         timer(start);
         isplaying = true;
     }
+    hasPlayed = document.getElementsByClassName('time-min');
+}
+
+function getBreak(){
+    timerDisplay.textContent = `${breakTime.innerText}:00`;
+    incWork.style.pointerEvents = 'none';
+    decWork.style.pointerEvents = 'none';
+    incBreak.style.pointerEvents = 'auto';
+    decBreak.style.pointerEvents = 'auto';
+
+    isWork = false;
+    isBreak = true;
+}
+
+function getWork(){
+    timerDisplay.textContent = `${workTime.innerText}:00`;
+    incBreak.style.pointerEvents = 'none';
+    decBreak.style.pointerEvents = 'none';
+    incWork.style.pointerEvents = 'auto';
+    decWork.style.pointerEvents = 'auto';
+
+    isWork = true
+    isBreak = false
+}
+
+function timesUp(){
+    const audio = new Audio('/home/william/the_odin_project/ptimer/sounds/TimesUp.wav');
+    audio.loop = false;
+    audio.play(); 
+    
 }
 
 incWork.addEventListener('click',() => incWorkTime());
@@ -96,3 +164,9 @@ decWork.addEventListener('click',() => decWorkTime());
 incBreak.addEventListener('click', () => incBreakTime());
 decBreak.addEventListener('click', () => decBreakTime());
 btn.addEventListener('click', () => startTimer());
+switchBreak.addEventListener('click', () => getBreak());
+switchWork.addEventListener('click', () => getWork());
+
+isWork = true;
+incBreak.style.pointerEvents = 'none';
+decBreak.style.pointerEvents = 'none';
